bda24dca54859171dd9fd3d4c19abfd22f20cd82
Wxueru <1019629064@qq.com>
2016-10-28 08:45:30 +0800

    引入lazyloadJS，添加案例、日记，但需要更替

---
+++ find/js/disugner.js
@@ -0,0 +1,5 @@
+app.controller("designerCtrl", function ($scope, $css) {
+
+    $css.removeAll();
+    $css.add('./find/css/designer.css');
+});
--- strategy/js/strategy-demo.js
+++ strategy/js/strategy-demo.js
@@ -1,12 +1,34 @@
 app.controller("strategy-demoCtrl", function ($scope, $css) {
     $css.removeAll();
     $css.add('./strategy/css/strategy-demo.css');
-    var hea = document.querySelector(".str-demo-header");
-    var indexHeader = document.querySelector(".index-header");
-    console.log(indexHeader);
-    // indexHeader.style.display = none;
-    console.log(hea);
-    window.onscroll = function(){
-        hea.style.background = "transparent";
-    }
+
+    //str-demo-header中的变化
+    $(function(){
+        $('.header').hide();
+        $(window).scroll(function(){
+            var top = $(window).scrollTop() * 0.0025;
+            $('.str-demo-header').css({'background':'rgba(255, 255, 255,' + top + ')'});
+            $('.str-demo-enjoy .str-demo-eimg1').show();
+            $('.str-demo-enjoy .str-demo-eimg2').hide();
+            $('.str-demo-header>div>span').css({'border-color':'white'});
+            $('.str-demo-enjoy').css({'color':'white'});
+            if($(window).scrollTop()>10){
+                $('.str-demo-header>div>span').css({'border-color':'#7db444'});
+                $('.str-demo-enjoy .str-demo-eimg1').hide();
+                $('.str-demo-enjoy .str-demo-eimg2').show();
+                $('.str-demo-enjoy').css({'color':'#7db444'});
+            }
+        });
+        $.each($('.str-demo-info'), function (index, value) {
+            $(value).find('div').click(function () {
+                $(value).find('.str-demo-star1').toggle();
+                $(value).find('.str-demo-star2').toggle();
+
+            });
+
+        });
+
+
+
+    });
 });
